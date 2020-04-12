5bf61c4389da775e369f0e8ee668208746693543
codefuncode <codefuncode@protonmail.com>
2020-04-12 02:34:42 -0400

    ajax funcionando

--- ProyectNew/index.php
+++ ProyectNew/index.php
@@ -4,12 +4,47 @@
     <meta charset="utf-8"/>
     <meta content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" name="viewport"/>
     <link href="css/w3.css" rel="stylesheet" type="text/css"/>
-    <title>
-      Document
-    </title>
+    <script src="js/js.js" type="text/javascript">
+    </script>
   </head>
-  <body>
-    <div class="w3-container">
+  <title>
+    Document
+  </title>
+</html>
+<body>
+  <h2>
+    The XMLHttpRequest Object
+  </h2>
+  <h3>
+    Start typing a name in the input field below:
+  </h3>
+  <p>
+    Suggestions:
+    <span id="txtHint">
+    </span>
+  </p>
+  <p>
+    First name:
+    <input id="txt1" onkeyup="showHint(this.value)" type="text"/>
+  </p>
+  <script>
+    function showHint(str) {
+  var xhttp;
+  if (str.length == 0) { 
+    document.getElementById("txtHint").innerHTML = "";
+    return;
+  }
+  xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("txtHint").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "php/php.php?q="+str, true);
+  xhttp.send();   
+}
+  </script>
+  <!-- <div class="w3-container">
       <h2>
         Navigation Bar with Input and Buttons
       </h2>
@@ -34,6 +69,5 @@
       <div class="">
        <?php include 'componentes/video1.html';?>
       </div>
-    </div>
-  </body>
-</html>
+    </div> -->
+</body>
--- ProyectNew/js/js.js
+++ ProyectNew/js/js.js
@@ -1,1 +1,15 @@
-js.js
+function showHint(str) {
+  if (str.length == 0) {
+    document.getElementById("txtHint").innerHTML = "";
+    return;
+  } else {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("txtHint").innerHTML = this.responseText;
+      }
+    };
+    xmlhttp.open("GET", "gethint.php?q=" + str, true);
+    xmlhttp.send();
+  }
+}
