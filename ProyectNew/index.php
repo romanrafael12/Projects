626719dee5efb46550c68b15cd9d0e86b350f4b6
codefuncode <codefuncode@protonmail.com>
2020-04-12 02:13:49 -0400

    Cambios

---
+++ ProyectNew/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8"/>
+    <meta content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" name="viewport"/>
+    <link href="css/w3.css" rel="stylesheet" type="text/css"/>
+    <title>
+      Document
+    </title>
+  </head>
+  <body>
+    <div class="w3-container">
+      <h2>
+        Navigation Bar with Input and Buttons
+      </h2>
+      <p>
+        Input and button inside navbar:
+      </p>
+      <div class="w3-bar w3-black w3-border">
+        <a class="w3-bar-item w3-button w3-mobile" href="#">
+          Home
+        </a>
+        <a class="w3-bar-item w3-button w3-mobile" href="#">
+          Link 1
+        </a>
+        <a class="w3-bar-item w3-button w3-mobile" href="#">
+          Link 2
+        </a>
+        <input class="w3-bar-item w3-input w3-white w3-mobile" placeholder="Search.." type="text"/>
+        <button class="w3-bar-item w3-button w3-green w3-mobile">
+          Go
+        </button>
+      </div>
+      <div class="">
+       <?php include 'componentes/video1.html';?>
+      </div>
+    </div>
+  </body>
+</html>
